14e46f490cb220d546b1abffc06a3978da8c56a5
kevinlee <kevinlee3618@gmail.com>
2021-02-27 20:00:46 -0800

    Fixed Variable Name

--- src/main/java/com/spring/studentDatabase/student/StudentConfig.java
+++ src/main/java/com/spring/studentDatabase/student/StudentConfig.java
@@ -13,19 +13,19 @@ public class StudentConfig {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository repository) {
         return args -> {
-            Student mariam = new Student(
+            Student student1 = new Student(
                     "John Doe",
                     "john.doe@gmail.com",
                     LocalDate.of(1990, Month.MARCH, 25)
             );
 
-            Student alex = new Student(
+            Student student2 = new Student(
                     "Jane Doe",
                     "jane.doe@gmail.com",
                     LocalDate.of(1995, Month.JULY, 15)
             );
 
-            repository.saveAll(List.of(mariam, alex));
+            repository.saveAll(List.of(student1, student2));
         };
     }
 }
